07d80aedb146ebcc1610516b6251dcd9edbe4a99
lopezrunco <lopezrunco@gmail.com>
2021-11-16 18:14:07 -0300

    Renderizado de todos en home. Componente card. Limpieza de codigo.

--- src/pages/Home/components/Card/index.js
+++ src/pages/Home/components/Card/index.js
@@ -1,5 +1,54 @@
-function Card() {
-    return <p>Card component</p>
+import './style.scss'
+import { Eye, Trash, Circle, CircleFill } from 'react-bootstrap-icons'
+
+function Card(props) {
+    const toggleTodoCompletion = () => {}
+
+    const viewTodo = () => {}
+
+    const deleteTodo = () => {}
+
+    return (
+        <div className="todo-card col">
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">{ props.todo.title }</h4>
+                    <p className="card-text">{ props.todo.description }</p>
+                </div>
+
+                <div className="card-floating-actions d-flex">
+                    <button
+                        type="button"
+                        className="btn-change-todo-status fab fab-mini me-2 d-flex justify-content-center align-items-center bg-primary"
+                        onClick={toggleTodoCompletion}
+                    >
+                        {/* Ternario para cambiar el icono dependiendo de si la tarea esta completa o no */}
+                        {props.todo.completed ? (
+                            <Circle color="white" />
+                        ) : (
+                            <CircleFill color="white" />
+                        )}
+                    </button>
+
+                    <button
+                        type="button"
+                        className="btn-view-todo fab fab-mini me-2 d-flex justify-content-center align-items-center bg-info"
+                        onClick={viewTodo}
+                    >
+                        <Eye color="white" />
+                    </button>
+
+                    <button
+                        type="button"
+                        className="btn-delete-todo fab fab-mini me-2 d-flex justify-content-center align-items-center bg-danger"
+                        onClick={deleteTodo}
+                    >
+                        <Trash color="white" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Card
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,145 @@
-import './style.css'
+import './style.scss'
+import React from 'react'
+import { Plus } from 'react-bootstrap-icons'
+import { AuthContext } from '../../App'
+import Card from './components/Card'
+import { apiUrl } from '../../utils/api-url'
+
+// Creacion de contexto de tareas
+export const TodoContext = React.createContext()
+
+// Creacion de elemento con filtros de categorias
+// La funcion freeze() congela los valores para que sean solo de lectura y no se modifiquen
+const filters = Object.freeze({
+    ALL: 'ALL',
+    DAY: 'DAY',
+    HIGH_PRIORITY: 'HIGH_PRIORITY'
+})
+
+// Estado inicial del componente
+const initialState = {
+    todos: [],
+    isFetching: false,
+    hasError: false
+}
+
+// Manejo de estados con un reducer
+const reducer = (state, action) => {
+    switch (action.type) {
+        // Lanzamiento de fetch para los todos
+        case 'FETCH_TODOS_REQUEST':
+            return {
+                ...state,
+                isFetching: true,
+                hasError: false
+            }
+        // Fetch exitoso
+        case 'FETCH_TODOS_SUCCESS':
+            return {
+                ...state,
+                isFetching: false,
+                todos: action.payload.todos // Setea los todos que llegaron en la peticion
+            }
+        // Fetch fallido
+        case 'FETCH_TODOS_FAILURE':
+            return {
+                ...state,
+                hasError: true,     // Hubo error
+                isFetching: false
+            }
+        default:
+            return state
+    }
+}
 
 function Home() {
-    return (
-        <main className="container mb-5">
+    // Se usa el contexto de autenticacion para acceder al toquen con el que se solicitan las todos
+    const { state: authState } = React.useContext(AuthContext)
+    // Hook de reducer con el estado inicial
+    // Deja disponible el dispatch para avisar al reducer si se disparo una accion
+    // Deja disponible el estado para usarlo en el componente para mostrar las todos
+    const [state, dispatch] = React.useReducer(reducer, initialState)
+
+    const search = () => { }
+
+    const applyFilter = () => { }
+
+    // Cuando se carga el componente obtiene los todos y los muestra (o al menos la primer pagina)
+    React.useEffect(() => {
+
+        // Si tiene token se hace la peticion de todos
+        if (authState.token) {
+            dispatch({
+                type: 'FETCH_TODOS_REQUEST'
+            })
+
+            // Peticion de las todos
+            fetch(apiUrl('todos'), {
+                headers: {
+                    'Authorization': authState.token, // Importante pasar el token
+                    'Content-Type': 'application/json'
+                },
+            }).then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw response
+                }
+            }).then(data => {
+                dispatch({
+                    // Si salio todo OK, se hace un dispatch de tipo success con las todos que vinieron
+                    type: 'FETCH_TODOS_SUCCESS',
+                    payload: data // Este data tiene los todos
+                })
+            }).catch(error => {
+                console.error(error)
+
+                dispatch({
+                    // Si fallo, se emite dispatch de tipo failure
+                    type: 'FETCH_TODOS_FAILURE'
+                })
+            })
+        }
+    }, [authState.token])   // El useEffect se volvera a disparar si el valor del token cambia
+
+    return ( 
+        <main className="page-home container mb-5">
             <div className="bg-light p-4 rounded">
                 <div className="input-group mb-4">
                     <input id="search-keywords" type="text" className="form-control" placeholder="Terminos de busqueda ..." />
-                    <button className ="btn btn-secondary" type ="button" onlick="applySearchFilter()">Buscar</button>
+                    <button className="btn btn-secondary" type="button" onClick={search}>Buscar</button>
                 </div>
 
                 <ul className="nav nav-pills nav-fill mb-4">
                     <li className="nav-item">
-                        <a className="nav-link active" onlick="applyTypeFilter('MY_DAY')">Mi día</a>
+                        <a className="nav-link" onClick={() => applyFilter(filters.DAY)}>Mi día</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" onlick="applyTypeFilter('IMPORTANT')">Importantes</a>
+                        <a className="nav-link" onClick={() => applyFilter(filters.HIGH_PRIORITY)}>Importantes</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" onlick="applyTypeFilter('ALL')">Todas</a>
+                        <a className="nav-link active" onClick={() => applyFilter(filters.ALL)}>Todas</a>
                     </li>
                 </ul>
 
-                <div id="todos-container" className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4" data-masonry='{ "percentPosition": true }'>
+                <div id="todos-container" className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                    {/* Ternario: si se esta haciendo la peticion, muestra Cargando...
+                        si no hace otro ternario que muestra error o muestra las tareas  */}
+                    {state.isFetching ? (
+                        <span className="loader">Cargando...</span>
+                    ) : state.hasError ? (
+                        <span className="error">Ocurrió un error</span>
+                    ) : (
+                        <>
+                            {/* Si hay tareas, mapea el array de tareas y genera un componente card por cada una */}
+                            {state.todos.length > 0 &&
+                                state.todos.map(todo => (
+                                    // Key se utiliza para facilitar el renderizado de elementos (al reordenar o volver a dibujar los elementos por ejemplo)
+                                    <Card key={todo.id} todo={todo} />
+                                ))
+                            }
+                        </>
+                    )}
                 </div>
 
                 <div id="create-new-todo-hint" className="d-none">
@@ -31,12 +149,12 @@ function Home() {
                 </div>
             </div>
 
-            <button 
+            <button
                 className="fab fab-fixed d-flex justify-content-center align-items-center bg-success"
-                onlick="openTodoCreatorModal()">
-                <i className="bi bi-plus"></i>
+                onClick={() => console.log('Open todo creator')}
+            >
+                <Plus color="white" size={30} />
             </button>
-
         </main>
     )
 }
