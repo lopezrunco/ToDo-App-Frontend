e5fa29d45949f28fadae288d76deee6e26075585
lopezrunco <lopezrunco@gmail.com>
2021-11-16 09:59:10 -0300

    Actualizacion de dependencias, readme e index.html

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Crear archivo `.env` con variables de entorno.
 
 Ejemplo:
 ```
+PORT=4000
 REACT_APP_API_PROTOCOL=http
 REACT_APP_API_HOST=localhost
 REACT_APP_API_PORT=3000
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Impotacion de tema de Bootswatch
+import 'bootswatch/dist/minty/bootstrap.min.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
