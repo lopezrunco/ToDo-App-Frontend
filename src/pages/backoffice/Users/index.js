36029295c18a6b96c14ed52ed0861372a91fdbc4
lopezrunco <lopezrunco@gmail.com>
2022-02-01 22:02:41 -0300

    Get all users

--- src/pages/backoffice/Users/index.js
+++ src/pages/backoffice/Users/index.js
@@ -1,9 +1,140 @@
+import { useContext, useEffect, useReducer } from 'react'
+
+import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from './action-types'
+import { refreshToken } from '../../../utils/refresh-token'
+import { apiUrl } from '../../../utils/api-url'
+import { AuthContext } from '../../../App'
+import { HIDE_LOADER, SHOW_LOADER } from '../../../action-types'
+
+const initialState = {
+    users: [],
+    isFetching: false,
+    hasError: false
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case FETCH_USERS_REQUEST:
+            return {
+                ...state,
+                isFetching: true,
+                hasError: false
+            }
+        case FETCH_USERS_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                users: action.payload.users
+            }
+        case FETCH_USERS_FAILURE:
+            return {
+                ...state,
+                hasError: true,
+                isFetching: false
+            }
+        default:
+            return state
+    }
+}
+
 function Users() {
+
+    const { state: authState, dispatch: authDispatch } = useContext(AuthContext)
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        if (authState.token) {
+            authDispatch({
+                type: SHOW_LOADER
+            })
+
+            dispatch({
+                type: FETCH_USERS_REQUEST
+            })
+
+            fetch(apiUrl('admin/users'), {
+                headers: {
+                    'Authorization': authState.token,
+                    'Content-Type': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw response
+                }
+            }).then(data => {
+                dispatch({
+                    type: FETCH_USERS_SUCCESS,
+                    payload: data
+                })
+            }).catch(error => {
+                console.error('Error fetching the users', error)
+
+                if (error.status === 401) {
+                    refreshToken(
+                        authState.refreshToken,
+                        authDispatch,
+                        navigate
+                    )
+                } else if (error.status === 403) {
+                    navigate('/forbidden')
+                } else {
+                    dispatch({
+                        type: FETCH_USERS_FAILURE
+                    })
+                }
+            }).finally(() => {
+                authDispatch({
+                    type: HIDE_LOADER
+                })
+            })
+        }
+    }, [authDispatch, authState.token, authState.refreshToken])
+
     return (
-        <div className="container">
-            <h2>Users page</h2>
-            {/* TODO: Get all users */}
-        </div>
+        <main className='select-opponent container'>
+            <div className="row">
+                <div className="col-12">
+                    {!state.hasError && (
+                        <div className='count-info'>
+                            <h2>Users:</h2>
+                            <h6>Count: {state.users.length}</h6>
+                        </div>
+                    )}
+                </div>
+
+                <div className="col-12">
+                    <div className='row'>
+                        {state.isFetching ? (
+                            <p>Loading</p>
+                        ) : state.hasError ? (
+                            <p>Error obtaining the users</p>
+                        ) : (
+                            <>
+                                {state.users.length > 0 ? (
+                                    state.users.map(user => (
+                                        <div className='col-md-6 mb-3'>
+                                            <ul className='list-group'>
+                                                <li className='list-group-item'><strong>ID:</strong> {user.id}</li>
+                                                <li className='list-group-item'><strong>Name:</strong> {user.name}</li>
+                                                <li className='list-group-item'><strong>Email:</strong> {user.email}</li>
+                                                <li className='list-group-item'><strong>Role:</strong> {user.role}</li>
+                                                <li className='list-group-item'><strong>MFA enabled:</strong> {user.mfaEnabled ? 'Yes' : 'No'}</li>
+                                            </ul>
+                                        </div>
+
+                                    ))
+                                ) : (
+                                    <p>No users</p>
+                                )}
+                            </>
+                        )}
+                    </div>
+                </div>
+
+            </div>
+        </main>
     )
 }
 
